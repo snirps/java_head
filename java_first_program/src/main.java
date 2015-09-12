162099d3155823c6f5d3498b965dbae88d1fc900
snirps <kajek89@gmail.com>
2015-09-12 21:14:15 +0200

    Add Java and Kotlin programs

--- java_first_program/src/main.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by Karol on 2015-09-12.
- */
-public class main {
-}
---
+++ kotlin_first_program/src/com/first/kotlin/main_kotlin.kt
@@ -0,0 +1,8 @@
+package com.first.kotlin
+
+/**
+ * Created by Karol on 2015-09-12.
+ */
+fun main(args: Array<String>) {
+    println("czolgaj sie piesku")
+}
