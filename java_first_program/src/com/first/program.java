60be4757768693195c2af4399856c1e2f7571fd6
snirps <kajek89@gmail.com>
2015-09-12 22:31:14 +0200

    add new line with text: "right now!!!"

--- java_first_program/src/com/first/program.java
+++ java_first_program/src/com/first/program.java
@@ -1,11 +1,9 @@
 package com.first;
 
-/**
- * Created by Karol on 2015-09-12.
- */
 public class program {
 
     public static void main(String[] args) {
         System.out.println("Come and dance with me");
+        System.out.print("Right now!!!");
     }
 }
